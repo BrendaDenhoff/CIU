f0de3a32decff2badee73f30bd690ef0c0ae7a77
Brenda Denhoff <brenda.denhoffstudiantes.unahur.edu.ar>
2021-10-09 18:52:01 -0300

    subiendo trabajos

---
+++ clase_2/ejercicio-javascript/punto1.js
@@ -0,0 +1,7 @@
+var nombre = "Brenda";
+var edad = 22;
+var cumple = "8 de julio";
+var ciudad = "San Miguel";
+var ocupacion = "estudiante";
+
+console.log(`Hola soy ${nombre}, tengo ${edad} años cumplo el ${cumple}. Soy de ${ciudad} y soy ${ocupacion}`);
